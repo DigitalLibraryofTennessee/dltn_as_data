e28bfce19dde8caac5711aeff8506c627655871c
Mark Baggett <mbagget1@utk.edu>
2023-05-12 09:01:34 -0400

    Pretty print on provider.

--- utilities/pretty_print.py
+++ utilities/pretty_print.py
@@ -4,15 +4,20 @@ import os
 class DirectoryOfFiles:
     def __init__(self, directory):
         self.directory = directory
+        self.sets = self.get_sets()
 
     def pretty_print_directory(self):
-        for file in os.listdir(self.directory):
-            tree = etree.parse(os.path.join(self.directory, file))
-            pretty_xml = etree.tostring(tree, pretty_print=True, encoding='unicode')
-            with open(os.path.join(self.directory, file), 'w') as file:
-                file.write(pretty_xml)
+        for set in self.sets:
+            for file in os.listdir(f'{self.directory}/{set}/MODS'):
+                tree = etree.parse(os.path.join(f'{self.directory}/{set}/MODS', file))
+                pretty_xml = etree.tostring(tree, pretty_print=True, encoding='unicode')
+                with open(os.path.join(f'{self.directory}/{set}/MODS', file), 'w') as file:
+                    file.write(pretty_xml)
+
+    def get_sets(self):
+        return [file for file in os.listdir(self.directory)]
 
 
 if __name__ == "__main__":
-    x = DirectoryOfFiles('providers/bcpl/p17194coll17/MODS')
+    x = DirectoryOfFiles('providers/memphis_public/')
     x.pretty_print_directory()
